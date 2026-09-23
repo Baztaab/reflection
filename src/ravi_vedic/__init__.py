da27bd13a01fd15d6869f5b16f99ec850479f9b3
Diba <mehraneskandari46@gmail.com>
2026-09-23 17:07:55 +0330

    M2.6.9.1: export typed engine errors

--- src/ravi_vedic/__init__.py
+++ src/ravi_vedic/__init__.py
@@ -4,10 +4,26 @@ from .application.engine import RaviEngine
 from .bootstrap import create_engine
 from .domain.canon import RAVI_VEDIC_MVP_V1
 from .domain.models import BirthInput, CoreResult
+from .errors import (
+    AstronomyBackendError,
+    InputError,
+    InvariantViolationError,
+    RaviError,
+    RuntimeDataError,
+    TimeResolutionError,
+    UnsupportedPolicyError,
+)
 from .runtime import RuntimeConfig, SourceProfile
 
 __all__ = [
+    "AstronomyBackendError",
+    "InputError",
+    "InvariantViolationError",
     "RAVI_VEDIC_MVP_V1",
+    "RaviError",
+    "RuntimeDataError",
+    "TimeResolutionError",
+    "UnsupportedPolicyError",
     "BirthInput",
     "CoreResult",
     "RaviEngine",
